e33927aad6d91556248a6765595a4eeb7a550505
Brovko Roman <brovko-roman@ya.ru>
2016-01-01 13:23:58 +0200

    add files

---
+++ AdaptiveUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveUI
+//
+//  Created by Игорь on 21.12.15.
+//  Copyright © 2015 Ihor Malovanyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
